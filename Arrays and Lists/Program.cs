577a9d7ee138cc9dd2c17cc6a0ed08904168f3b0
Yacov Rosenberg <yacov94@yahoo.com>
2019-09-09 22:26:34 -0300

    first commit

---
+++ Arrays and Lists/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Arrays_and_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = new CircularLinkedList<int>();
+            lista.AddLast(0);
+            lista.AddFirst(1);
+            lista.AddLast(7);
+            lista.AddLast(9);
+
+            foreach(int i in lista)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
